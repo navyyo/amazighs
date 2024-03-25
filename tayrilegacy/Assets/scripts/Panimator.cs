aebca8cacfeddcca6b2e4a0f9111d7fd892b48b5
navyyo <yomnarabeh@gmail.com>
2024-03-25 16:06:32 +0100

    25/4

---
+++ tayrilegacy/Assets/scripts/Panimator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panimation : MonoBehaviour
+{
+    Panimation animator;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
