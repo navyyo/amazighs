9905a44b45b4eddc7b149185809b370da180b26e
eyabenbecher <150703203+eyabenbecher@users.noreply.github.com>
2024-04-15 16:05:37 +0100

    code progress

--- tayrilegacy/Assets/scripts/ClearCounter.cs
+++ tayrilegacy/Assets/scripts/ClearCounter.cs
@@ -1,13 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ClearCounter : MonoBehaviour
+
 {
-    [SerializeField] private Transform tomatoPrefab;
-    [SerializeField] private Vector3 spawnPosition; // Define the spawn position
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Vector3 spawnPosition;
+    private KitchenObject kitchenObject;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private void Update()
+    {
+        if(testing && Input.GetKeyUp(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+                Debug.Log(kitchenObject.GetClearCounter());
+            }
+        }
+    }
     public void Connect()
     {
-        Debug.Log("connect!");
-        Transform tomatoTransform = Instantiate(tomatoPrefab, spawnPosition, Quaternion.identity);
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, spawnPosition, Quaternion.identity);
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+             kitchenObject.SetClearCounter(this);
+         }
+         else { Debug.Log(kitchenObject.GetClearCounter()); }
     }
+  
+
 }
 
