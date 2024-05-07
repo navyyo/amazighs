1e892d3047511f6cdf1058f68f4789d649570f2a
navyyo <yomnarabeh@gmail.com>
2024-05-07 01:45:51 +0100

    just a text bubble 🎀

---
+++ tayrilegacy/Assets/scripts/languageSystem/GlyphsInteraction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlyphsInteraction : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("interact");
+    }
+}
